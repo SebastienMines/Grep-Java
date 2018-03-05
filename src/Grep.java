266c547623e9303faf8b818d47851ff558279b91
Sébastien MINES <sebastien.mines@gmail.com>
2018-03-05 12:19:18 +0100

    Push du ReadFile + file.txt

--- src/Grep.java
+++ src/Grep.java
@@ -1,4 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Grep {
+	
 
+	public void splitText(String word, String text) {
+		// TODO Auto-generated method stub
+		ArrayList<String> result = new ArrayList<>();
+		
+		
+	}
+	
+	public void readFile(String file) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		try {
+		    StringBuilder sb = new StringBuilder();
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        sb.append(line);
+		        sb.append(System.lineSeparator());
+		        line = br.readLine();
+		    }
+		    String everything = sb.toString();
+		    System.out.println(everything);
+		} finally {
+		    br.close();
+		}
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		String word = "T";
+		String text = "Taratata";
+		
+		Grep grep = new Grep();
+		grep.readFile("file.txt");
 	}
 
 }

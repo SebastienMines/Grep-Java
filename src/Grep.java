607c4d108999071e9160caec6867564db42b6ed5
Allan77230 <predaboss@gmail.com>
2018-03-06 11:54:48 +0100

    fichier html + adaptation

--- src/Grep.java
+++ src/Grep.java
@@ -9,11 +9,36 @@ public class Grep {
 		
 		//String fileName = args[0];
 		String fileName = new String("file.txt");
+		String fileNameHtml = new String("fileHtml.txt");
+
 		grep(fileName);
+		grepHtml(fileNameHtml);
+
 		
 	}
 
 	private static void grep(String fileName) throws IOException {
+		
+		String line;
+		Reader input = new FileReader(new File(fileName));
+		LineNumberReader inputStream = new LineNumberReader(input);
+		char[][] automata = new char[5][5];
+		MyGrep m = new MyGrep(automata);
+		
+		while ((line = inputStream.readLine()) != null) {
+			
+			if (m.find(line)) {
+				System.out.println(inputStream.getLineNumber() + " : " + line);
+			}
+			
+		}
+		
+		inputStream.close();
+		input.close();
+	}
+	
+	
+	private static void grepHtml(String fileName) throws IOException {
 		
 		String line;
 		Reader input = new FileReader(new File(fileName));
